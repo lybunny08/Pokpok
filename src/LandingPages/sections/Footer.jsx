02860dba657961278143159dcb7360d813f5482a
Lydien Mamitiana <mamitiana22.aps2a@gmail.com>
2025-12-18 15:26:43 +0300

    fix apply to Apply on Assets that cause issues on vercel

--- src/LandingPages/sections/Footer.jsx
+++ src/LandingPages/sections/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Newsletters from '../../components/NewsLetters';
 import body from '../../assets/body.webp'
 import men from '../../assets/men.webp'
-import apply from '../../assets/apply.webp'
+import apply from '../../assets/Apply.webp'
 import shower from '../../assets/Wet_portrait.png'
 import women from '../../assets/women.webp'
 
