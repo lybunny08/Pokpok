2c54ff19a812005f1a03dbbb1eeb4cd3650fb46e
Lydien Mamitiana <mamitiana22.aps2a@gmail.com>
2025-12-21 12:48:00 +0300

    add boutton togoHome on NotFound page

--- src/shared/layout/NotFound.jsx
+++ src/shared/layout/NotFound.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import ButtonComponents from '../../LandingPages/components/ui/ButtonComponents'
+import { useNavigate } from 'react-router-dom';
+import ButtonComponents from '../../LandingPages/components/ui/ButtonComponents';
 
 function NotFound() {
+    const navigate = useNavigate();
+    
+    const handleGoHome = () => {
+        navigate('/');
+    };
+
     return (
-        <div className='flex flex-col justify-center item-center gap-[32px] pt-[200px] '>
-            <span>Oops! You're lost.</span>
+        <div className='flex flex-col justify-center items-center gap-[8px] pt-[100px] '>
+            <span className='text-[40px] font-medium '>Oops! You're lost.</span>
             <span>The page you are looking for was not found or relocated.</span>
-            <ButtonComponents>Return Home</ButtonComponents>
+            <ButtonComponents 
+                className='mt-4' 
+                textColor='black' 
+                borderColor='black' 
+                text="Return Home"
+                onClick={handleGoHome}
+            />
         </div>
     );
 }
